ed4877cc4df695c2aac282d92670df24859885d9
Lenz Paul <bringbackthedog@gmail.com>
2023-04-08 16:26:49 -0400

    remove outer app folder - now plugin only

--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:web_workers/web_workers.dart';
-
-// void main() {
-//   //
-//   // var ww = WebWorkersPlugin
-// }
--- plugins/web_workers/README.md
+++
@@ -1,3 +0,0 @@
-# web_workers
-
-This plugin provides a way to run Dart code in a separate web worker on web.
